7ad3064cc2219b015d5c479fa71487fe5970fd91
SBennett <SBennett@datavizion.com>
2025-04-23 10:59:26 -0500

    Removed two Unfinished Unsophisticated Scripts

--- PingLogger.ps1
+++
@@ -1,6 +0,0 @@
-#Script to Log dropped pings
-
-New-Item -Path "C:\" -Name "Datavizion" -ItemType "Directory"
-Start-Transcript 'C:\Datavizion\ping_results.txt' #Create and begin logging to a results.txt
-ping -t teams.microsoft.com | Foreach{"{0} - {1}" -f (Get-Date -f "yyyyMMdd HH:mm:ss"),$_}  #Ping a host continuously and log them all with a timestamp
-Stop-Transcript #Stop transcription
